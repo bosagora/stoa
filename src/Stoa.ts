6f1b8ba13e3d265d9dc7363a736816ad26950ed1
Hyeonyeob Kim <kimhyunb@gmail.com>
2020-06-18 17:27:44 +0900

    Add post /push API URL

--- src/Stoa.ts
+++ src/Stoa.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 class Stoa {
     public stoa: express.Application;
 
     constructor () {
         this.stoa = express();
+        // parse application/x-www-form-urlencoded
+        this.stoa.use(bodyParser.urlencoded({ extended: false }))
+        // parse application/json
+        this.stoa.use(bodyParser.json())
 
         this.stoa.get("/validators",
             (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -14,6 +19,12 @@ class Stoa {
         this.stoa.get("/validator/:address",
             (req: express.Request, res: express.Response, next: express.NextFunction) => {
             res.send(req.params.address + ':' + req.query.height);
+        });
+
+        this.stoa.post("/push",
+            (req: express.Request, res: express.Response, next: express.NextFunction) => {
+            console.log(req.body);
+            res.status(200).send();
         });
     }
 }
