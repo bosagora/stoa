7e0da94091aab374d667850f4e6cfee0b6932dd1
Hyeonyeob Kim <kimhyunb@gmail.com>
2020-06-23 15:42:31 +0900

    Store JSON data of API POST 'push'
    Parse json data and save it in block storage.
    This operation will be asynchronous.

--- src/Stoa.ts
+++ src/Stoa.ts
@@ -1,15 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { LedgerStorage } from "./modules/storage/LedgerStorage";
 
 class Stoa {
     public stoa: express.Application;
 
-    constructor () {
+    public ledger_storage: LedgerStorage;
+
+    constructor (file_name: string) {
         this.stoa = express();
         // parse application/x-www-form-urlencoded
         this.stoa.use(bodyParser.urlencoded({ extended: false }))
         // parse application/json
         this.stoa.use(bodyParser.json())
+        // create blockStorage
+        this.ledger_storage = new LedgerStorage(file_name, (err: Error | null) =>
+        {
+            if (err != null)
+            {
+                console.error(err);
+                throw new Error(err.message);
+            }
+        });
 
         this.stoa.get("/validators",
             (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -21,10 +33,43 @@ class Stoa {
             res.send(req.params.address + ':' + req.query.height);
         });
 
+        /**
+         * When a request is received through the `/push` handler
+         * JSON block data is parsed and stored on each storage.
+         */
         this.stoa.post("/push",
             (req: express.Request, res: express.Response, next: express.NextFunction) => {
-            console.log(req.body);
-            res.status(200).send();
+
+            var block: any;
+            if (req.body.block == undefined)
+            {
+                res.status(400).send("Missing 'block' object in body");
+                return;
+            }
+
+            try {
+                block = JSON.parse(req.body.block);
+            } catch(e) {
+                res.status(400).send("Not a valid JSON format");
+                return;
+            }
+
+            console.log(block);
+
+            this.ledger_storage.putBlocks(block, (err: any) =>
+            {
+                if (err != null)
+                {
+                    console.error("Failed to store the payload of a push to the DB: " + err);
+                    res.status(500).send("An error occurred while saving");
+                    return;
+                }
+                else
+                {
+                    res.status(200).send();
+                    return;
+                }
+            });
         });
     }
 }
--- src/modules/storage/LedgerStorage.ts
+++ src/modules/storage/LedgerStorage.ts
@@ -1,6 +1,6 @@
 /*******************************************************************************
 
-    The class that insert and read the block into the database.
+    The class that create and insert and read the ledger into the database.
 
     Now only block header are stored.
     Transactions and other data will be managed by different classes.
@@ -17,30 +17,32 @@
 import { Storages } from './Storages';
 
 /**
- * The class that insert and read the block into the database.
+ * The class that insert and read the ledger into the database.
  */
-export class BlockStorage extends Storages
+export class LedgerStorage extends Storages
 {
     /**
-     * Creates a table. table name is `block`.
+     * Creates tables related to the ledger.
      * @param callback If provided, this function will be called when
      * the database was finished successfully or when an error occurred.
      * The first argument is an error object.
      */
-    public createTable (callback?: any)
+    public createTables (callback: (err: Error | null) => void)
     {
         var sql =
         `CREATE TABLE IF NOT EXISTS blocks
         (
-            height INTEGER PRIMARY KEY,
-            prev_block TEXT,
-            validators TEXT,
-            merkle_root TEXT,
-            signature TEXT
+            height INTEGER NOT NULL PRIMARY KEY,
+            prev_block TEXT NOT NULL,
+            validators TEXT NOT NULL,
+            merkle_root TEXT NOT NULL,
+            signature TEXT,
+            tx_count INTEGER NOT NULL,
+            enrollment_count INTEGER NOT NULL
         )`;
-        this.db.run(sql, (err: any) =>
+        this.db.run(sql, (err: Error | null) =>
         {
-            if (callback != undefined)
+            if (callback != null)
                 callback(err);
         });
     }
@@ -52,7 +54,7 @@ export class BlockStorage extends Storages
      * the database was finished successfully or when an error occurred.
      * The first argument is an error object.
      */
-    public put (data: any, callback?: any)
+    public putBlocks (data: any, callback: (err: Error | null) => void)
     {
         if (
                 (data == null) ||
@@ -63,33 +65,34 @@ export class BlockStorage extends Storages
                 (data.header.validators == undefined) ||
                 (data.header.validators._storage == undefined) ||
                 (data.header.merkle_root == undefined) ||
-                (data.header.signature == undefined)
+                (data.header.signature == undefined) ||
+                (data.txs == undefined)
         ) {
-            if (callback != undefined)
-                callback("Parameter validation failed.");
+            if (callback != null)
+                callback(new Error("Parameter validation failed."));
             return;
         }
 
+        var tx_count: number = data.txs.length;
+        var enrollment_count: number = data.enrollments? data.enrollments.length: 0;
+
         var sql =
-        `INSERT INTO blocks
-            (height, prev_block, validators, merkle_root, signature)
-        VALUES
-            (?, ?, ?, ?, ?)
-        ON CONFLICT(height) DO UPDATE SET
-            prev_block = prev_block,
-            validators = validators,
-            merkle_root = merkle_root,
-            signature = signature`;
+            `INSERT INTO blocks
+                (height, prev_block, validators, merkle_root, signature, tx_count, enrollment_count)
+            VALUES
+                (?, ?, ?, ?, ?, ?, ?)`;
         this.db.run(sql,
             [
                 data.header.height.value,
                 data.header.prev_block,
                 JSON.stringify(data.header.validators._storage),
                 data.header.merkle_root,
-                data.header.signature
-            ], (err: any) =>
+                data.header.signature,
+                tx_count,
+                enrollment_count
+            ], (err: Error | null) =>
         {
-            if (callback != undefined)
+            if (callback != null)
                 callback(err);
         });
     }
@@ -102,20 +105,17 @@ export class BlockStorage extends Storages
      * The first argument is an error object.
      * The second argument is result set.
      */
-    public get (height: any, callback?: any)
+    public getBlocks (height: any, callback: (err: Error | null, rows: any[]) => void)
     {
         var sql =
         `SELECT
-            height, prev_block, validators, merkle_root, signature
+            height, prev_block, validators, merkle_root, signature, tx_count, enrollment_count
         FROM
             blocks
         WHERE height = ?`;
-        this.db.all(sql, [height], (err: any, rows: any) =>
+        this.db.all(sql, [height], (err: Error | null, rows: any[]) =>
         {
-            if (callback != undefined)
-                callback(err, rows);
-            else
-                console.log(callback);
+            callback(err, rows);
         });
     }
 
