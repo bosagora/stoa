59f53dd43b15ac8a2f102dfeb900c96ee7b38229
Hyeonyeob Kim <kimhyunb@gmail.com>
2020-06-25 16:50:04 +0900

    Modifying wording when Stoa API server is listening

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@ import express from "express";
 const port: number = Number(process.env.PORT) || 3836;
 const stoa: express.Application = new Stoa("database").stoa;
 
-stoa.listen(port, () => console.log(`Express server listening at ${port}`))
+stoa.listen(port, () => console.log(`Stoa API server listening at ${port}`))
 .on('error', err => console.error(err));
