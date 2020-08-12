163cafba0c7b69b2b6f774deaae658fcf3c6a06e
Hyeonyeob Kim <kimhyunb@gmail.com>
2020-08-12 15:42:18 +0900

    Add a new Logger Class
    Logger class is based on 'winston'.
    
    It operates as test, development, and production
    according to the environment variables 'process.env.NODE_ENV' mode.

---
+++ src/modules/common/Logger.ts
@@ -0,0 +1,78 @@
+/*******************************************************************************
+
+    Logging class for Stoa
+
+    Define the output format of Log and support console output
+    and file writing, depending on the 'NODE_ENV'.
+
+    Copyright:
+        Copyright (c) 2020 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+import winston from "winston";
+import { format } from "winston";
+import appRoot from "app-root-path";
+
+const { combine, timestamp, label, printf } = format;
+const logFormat = printf(({ level, message, label, timestamp }) => {
+  return `[${label}] ${timestamp} ${level} ${message}`;
+});
+
+const options = {
+    // write log file options
+    file: {
+      filename: `${appRoot}/logs/Stoa.log`, // TODO: move to config
+      handleExceptions: true,
+      json: false,
+      maxsize: 10485760, // 10MB
+      maxFiles: 10,
+      colorize: false,
+      format: combine(
+        label({ label: 'Stoa' }),
+        timestamp(),
+        logFormat
+      )
+    },
+    // console log mode options
+    console: {
+      handleExceptions: true,
+      json: false,
+      colorize: false,
+      format: combine(
+        label({ label: 'Stoa' }),
+        timestamp(),
+        logFormat
+      )
+    }
+};
+
+function createLogger() {
+    switch (process.env.NODE_ENV) {
+        case "test":
+            return winston.createLogger({
+                silent: true,
+            });
+        case "development":
+            return winston.createLogger({
+                level: "debug",
+                transports: [
+                    new winston.transports.Console(options.console)
+                ],
+            });
+        case "production":
+        default:
+            return winston.createLogger({
+                level: "info",
+                transports: [
+                    new winston.transports.File(options.file)
+                ],
+            });
+    }
+}
+
+export const logger = createLogger();
