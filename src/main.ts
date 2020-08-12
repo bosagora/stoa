2bb350b5980149ad933a304c3eebe433cf305906
Hyeonyeob Kim <kimhyunb@gmail.com>
2020-08-12 15:42:18 +0900

    Add a new Logger Class
    Logger class is based on 'winston'.
    
    It operates as test, development, and production
    according to the environment variables 'process.env.NODE_ENV' mode.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import 'source-map-support/register';
 import Stoa from './Stoa';
 import express from "express";
+import { logger } from "./modules/common/Logger";
 
 const port: number = Number(process.env.PORT) || 3836;
 const stoa: express.Application = new Stoa("database", "127.0.0.1", "2826").stoa;
 
-stoa.listen(port, () => console.log(`Stoa API server listening at ${port}`))
-.on('error', err => console.error(err));
+stoa.listen(port, () => logger.info(`Stoa API server listening at ${port}`))
+.on('error', err => logger.error(err));
