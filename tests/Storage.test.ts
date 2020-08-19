9f9eb03d53d44f097fbcc3a6f5dbd1d960ba7a72
Michael Kim <michael.kim.200518@gmail.com>
2020-08-19 13:20:11 +0900

    Apply Promise to test code

--- tests/Storage.test.ts
+++ tests/Storage.test.ts
@@ -26,19 +26,14 @@ function runLedgerStorageTest (doneIt: () => void)
     {
         assert.ok(!err1, err1?.message);
 
-        runBlockTest(ledger_storage, () =>
+        assert.doesNotThrow(async () =>
         {
-            runTransactionsTest(ledger_storage, () =>
-            {
-                runEnrollmentsTest(ledger_storage, () =>
-                {
-                    runValidatorsAPITest(ledger_storage, () =>
-                    {
-                        ledger_storage.close();
-                        doneIt();
-                    });
-                });
-            });
+            await runBlockTest(ledger_storage);
+            await runTransactionsTest(ledger_storage);
+            await runEnrollmentsTest(ledger_storage);
+            await runValidatorsAPITest(ledger_storage);
+            ledger_storage.close();
+            doneIt();
         });
     });
 }
@@ -46,37 +41,43 @@ function runLedgerStorageTest (doneIt: () => void)
 /**
  * Puts all data
  */
-function runBlockTest (ledger_storage: LedgerStorage, callback: () => void)
+function runBlockTest (ledger_storage: LedgerStorage): Promise<void>
 {
-    putAllBlockData(ledger_storage, (err2: Error | null) =>
+    return new Promise<void>((resolve, reject) =>
     {
-        assert.ok(!err2, err2?.message);
-
-        ledger_storage.getBlocks(1)
-            .then((rows: any[]) =>
+        putAllBlockData(ledger_storage)
+            .then(() =>
             {
-                assert.strictEqual(rows.length, 1);
-                assert.strictEqual(rows[0].height, 1);
-                assert.strictEqual(rows[0].merkle_root,
-                    '0x9c4a20550ac796274f64e93872466ebb551ba2cd3f2f051533d07a478d2402b' +
-                    '59e5b0f0a2a14e818b88007ec61d4a82dc9128851f43799d6c1dc0609fca1537d');
-                    callback();
+                ledger_storage.getBlocks(1)
+                    .then((rows: any[]) =>
+                    {
+                        assert.strictEqual(rows.length, 1);
+                        assert.strictEqual(rows[0].height, 1);
+                        assert.strictEqual(rows[0].merkle_root,
+                            '0x9c4a20550ac796274f64e93872466ebb551ba2cd3f2f051533d07a478d2402b' +
+                            '59e5b0f0a2a14e818b88007ec61d4a82dc9128851f43799d6c1dc0609fca1537d');
+                        resolve();
+                    })
+                    .catch((err) =>
+                    {
+                        assert.ok(!err, err);
+                        reject(err);
+                    });
             })
             .catch((err) =>
             {
-                assert.ok(!err, err?.message);
-                callback();
+                assert.ok(!err, err);
+                reject(err);
             });
-        });
+    });
 }
 
 /**
  * Puts all data
  */
-function putAllBlockData (ledger_storage: LedgerStorage,
-    callback: (err: Error | null) => void)
+function putAllBlockData (ledger_storage: LedgerStorage): Promise<void>
 {
-    (async () =>
+    return new Promise<void>(async (resolve, reject) =>
     {
         try
         {
@@ -85,153 +86,162 @@ function putAllBlockData (ledger_storage: LedgerStorage,
         }
         catch (error)
         {
-            callback(error);
+            reject(error);
             return;
         }
-        callback(null);
-    })();
+        resolve();
+    });
 }
 
 /**
  *  Run the test of the Enrollments
  */
-function runEnrollmentsTest (ledger_storage: LedgerStorage, onDone: () => void)
+function runEnrollmentsTest (ledger_storage: LedgerStorage): Promise<void>
 {
-    let height: number = 0;
-    ledger_storage.getEnrollments(height)
-        .then((rows: any[]) =>
-        {
-            assert.strictEqual(rows.length, 3);
-            assert.strictEqual(rows[0].block_height, height);
-            assert.strictEqual(rows[0].utxo_key,
-              '0x210b66053c73e7bd7b27673706f0272617d09b8cda76605e91ab66ad1cc3b' +
-              'fc1f3f5fede91fd74bb2d2073de587c6ee495cfb0d981f03a83651b48ce0e576a1a');
+    return new Promise<void>((resolve, reject) =>
+    {
+        let height: number = 0;
+        ledger_storage.getEnrollments(height)
+            .then((rows: any[]) =>
+            {
+                assert.strictEqual(rows.length, 3);
+                assert.strictEqual(rows[0].block_height, height);
+                assert.strictEqual(rows[0].utxo_key,
+                '0x210b66053c73e7bd7b27673706f0272617d09b8cda76605e91ab66ad1cc3b' +
+                'fc1f3f5fede91fd74bb2d2073de587c6ee495cfb0d981f03a83651b48ce0e576a1a');
 
-            ledger_storage.getValidators(height)
-                .then((rows: any[]) =>
-                {
-                    assert.strictEqual(rows.length, 3);
-                    assert.strictEqual(rows[0].enrolled_at, height);
-                    assert.strictEqual(rows[0].utxo_key,
-                      '0x210b66053c73e7bd7b27673706f0272617d09b8cda76605e91ab66ad1cc3b' +
-                      'fc1f3f5fede91fd74bb2d2073de587c6ee495cfb0d981f03a83651b48ce0e576a1a');
-                    assert.strictEqual(rows[0].address,
-                      'GA3DMXTREDC4AIUTHRFIXCKWKF7BDIXRWM2KLV74OPK2OKDM2VJ235GN');
-                      onDone();
-                })
-                .catch((err4) =>
-                {
-                    assert.ok(!err4, err4?.message);
-                    onDone();
-                });
-        })
-        .catch((err3) =>
-        {
-            assert.ok(!err3, err3?.message);
-            onDone();
-        });
+                ledger_storage.getValidators(height)
+                    .then((rows: any[]) =>
+                    {
+                        assert.strictEqual(rows.length, 3);
+                        assert.strictEqual(rows[0].enrolled_at, height);
+                        assert.strictEqual(rows[0].utxo_key,
+                        '0x210b66053c73e7bd7b27673706f0272617d09b8cda76605e91ab66ad1cc3b' +
+                        'fc1f3f5fede91fd74bb2d2073de587c6ee495cfb0d981f03a83651b48ce0e576a1a');
+                        assert.strictEqual(rows[0].address,
+                        'GA3DMXTREDC4AIUTHRFIXCKWKF7BDIXRWM2KLV74OPK2OKDM2VJ235GN');
+                        resolve();
+                    })
+                    .catch((err) =>
+                    {
+                        assert.ok(!err, err);
+                        reject(err);
+                    });
+            })
+            .catch((err) =>
+            {
+                assert.ok(!err, err);
+                reject(err);
+            });
+    });
 }
 
 /**
  * Run the test of the Transactions
  */
-function runTransactionsTest (ledger_storage: LedgerStorage, onDone: () => void)
+function runTransactionsTest (ledger_storage: LedgerStorage): Promise<void>
 {
-  ledger_storage.getTransactions(0)
-      .then((rows3: any[]) =>
-      {
-          assert.strictEqual(rows3.length, 4);
-          assert.strictEqual(rows3[0].tx_hash,
-              '0x3a245017fee266f2aeacaa0ca11171b5825d34814bf1e33fae76cca50751e5c' +
-              'fb010896f009971a8748a1d3720e33404f5a999ae224b54f5d5c1ffa345c046f7');
+    return new Promise<void>((resolve, reject) =>
+    {
+        ledger_storage.getTransactions(0)
+            .then((rows3: any[]) =>
+            {
+                assert.strictEqual(rows3.length, 4);
+                assert.strictEqual(rows3[0].tx_hash,
+                    '0x3a245017fee266f2aeacaa0ca11171b5825d34814bf1e33fae76cca50751e5c' +
+                    'fb010896f009971a8748a1d3720e33404f5a999ae224b54f5d5c1ffa345c046f7');
 
-          ledger_storage.getTxInputs(1, 0)
-              .then((rows4: any[]) =>
-              {
-                  assert.strictEqual(rows4.length, 1);
-                  assert.strictEqual(rows4[0].previous,
-                      '0x5d7f6a7a30f7ff591c8649f61eb8a35d034824ed5cd252c2c6f10cdbd223671' +
-                      '3dc369ef2a44b62ba113814a9d819a276ff61582874c9aee9c98efa2aa1f10d73');
-
-                  ledger_storage.getTxOutputs(0, 1)
-                      .then((rows5: any[]) =>
-                      {
-                          assert.strictEqual(rows5.length, 8);
-                          assert.strictEqual(rows5[0].utxo_key,
-                              '0xef81352c7436a19d376acf1eb8f832a28c6229885aaa4e3bd8c11d5d072e160' +
-                              '798a4ff3a7565b66ca2d0ff755f8cc0f1f97e049ca23b615c85f77fb97d7919b4');
-                          assert.strictEqual(rows5[0].tx_hash,
+                ledger_storage.getTxInputs(1, 0)
+                    .then((rows4: any[]) =>
+                    {
+                        assert.strictEqual(rows4.length, 1);
+                        assert.strictEqual(rows4[0].previous,
                             '0x5d7f6a7a30f7ff591c8649f61eb8a35d034824ed5cd252c2c6f10cdbd223671' +
                             '3dc369ef2a44b62ba113814a9d819a276ff61582874c9aee9c98efa2aa1f10d73');
-                          assert.strictEqual(rows5[0].address, 'GCOQEOHAUFYUAC6G22FJ3GZRNLGVCCLESEJ2AXBIJ5BJNUVTAERPLRIJ');
-                          assert.strictEqual(rows5[0].used, 1);
-                          onDone();
-                      })
-                      .catch((err5) =>
-                      {
-                          assert.ok(!err5, err5?.message);
-                          onDone();
-                      });
-              })
-              .catch((err4) =>
-              {
-                  assert.ok(!err4, err4?.message);
-                  onDone();
-              });
-      })
-      .catch((err3) =>
-      {
-          assert.ok(!err3, err3?.message);
-          onDone();
-      });
+
+                        ledger_storage.getTxOutputs(0, 1)
+                            .then((rows5: any[]) =>
+                            {
+                                assert.strictEqual(rows5.length, 8);
+                                assert.strictEqual(rows5[0].utxo_key,
+                                    '0xef81352c7436a19d376acf1eb8f832a28c6229885aaa4e3bd8c11d5d072e160' +
+                                    '798a4ff3a7565b66ca2d0ff755f8cc0f1f97e049ca23b615c85f77fb97d7919b4');
+                                assert.strictEqual(rows5[0].tx_hash,
+                                    '0x5d7f6a7a30f7ff591c8649f61eb8a35d034824ed5cd252c2c6f10cdbd223671' +
+                                    '3dc369ef2a44b62ba113814a9d819a276ff61582874c9aee9c98efa2aa1f10d73');
+                                assert.strictEqual(rows5[0].address, 'GCOQEOHAUFYUAC6G22FJ3GZRNLGVCCLESEJ2AXBIJ5BJNUVTAERPLRIJ');
+                                assert.strictEqual(rows5[0].used, 1);
+                                resolve();
+                            })
+                            .catch((err) =>
+                            {
+                                assert.ok(!err, err);
+                                reject(err);
+                            });
+                    })
+                    .catch((err) =>
+                    {
+                        assert.ok(!err, err);
+                        reject(err);
+                    });
+            })
+            .catch((err) =>
+            {
+                assert.ok(!err, err);
+                reject(err);
+            });
+    });
 }
 
 
 /**
 * Run Validators API tests
 */
-function runValidatorsAPITest (ledger_storage: LedgerStorage, onDone: () => void)
+function runValidatorsAPITest (ledger_storage: LedgerStorage): Promise<void>
 {
-    let address: string = 'GA3DMXTREDC4AIUTHRFIXCKWKF7BDIXRWM2KLV74OPK2OKDM2VJ235GN';
-    ledger_storage.getValidatorsAPI(1, null)
-        .then((rows: any[]) =>
-        {
-            assert.strictEqual(rows[0].address, address);
-            assert.strictEqual(rows[0].enrolled_at, 0);
-            assert.strictEqual(rows[0].distance, undefined);
+    return new Promise<void>((resolve, reject) =>
+    {
+        let address: string = 'GA3DMXTREDC4AIUTHRFIXCKWKF7BDIXRWM2KLV74OPK2OKDM2VJ235GN';
+        ledger_storage.getValidatorsAPI(1, null)
+            .then((rows: any[]) =>
+            {
+                assert.strictEqual(rows[0].address, address);
+                assert.strictEqual(rows[0].enrolled_at, 0);
+                assert.strictEqual(rows[0].distance, undefined);
 
-            ledger_storage.getValidatorsAPI(1, address)
-                .then((rows: any[]) =>
-                {
-                    assert.strictEqual(rows.length, 1);
-                    assert.strictEqual(rows[0].address, address);
-                    assert.strictEqual(rows[0].stake, '0x210b66053c73e7bd7b27673706f0272617d09b8cda76605e91ab66ad'+
-                    '1cc3bfc1f3f5fede91fd74bb2d2073de587c6ee495cfb0d981f03a83651b48ce0e576a1a');
+                ledger_storage.getValidatorsAPI(1, address)
+                    .then((rows: any[]) =>
+                    {
+                        assert.strictEqual(rows.length, 1);
+                        assert.strictEqual(rows[0].address, address);
+                        assert.strictEqual(rows[0].stake, '0x210b66053c73e7bd7b27673706f0272617d09b8cda76605e91ab66ad'+
+                        '1cc3bfc1f3f5fede91fd74bb2d2073de587c6ee495cfb0d981f03a83651b48ce0e576a1a');
 
-                    ledger_storage.getValidatorsAPI(Number.NaN, null)
-                        .then((rows: any[]) =>
-                        {
-                            assert.strictEqual(rows.length, 3);
-                            assert.strictEqual(rows[0].distance, undefined);
-                            onDone();
-                        })
-                        .catch((err3) =>
-                        {
-                            assert.ok(!err3, err3?.message);
-                            onDone();
-                        });
-                })
-                .catch((err2) =>
-                {
-                    assert.ok(!err2, err2?.message);
-                    onDone();
-                });
-        })
-        .catch((err1) =>
-        {
-            assert.ok(!err1, err1?.message);
-            onDone();
-        });
+                        ledger_storage.getValidatorsAPI(Number.NaN, null)
+                            .then((rows: any[]) =>
+                            {
+                                assert.strictEqual(rows.length, 3);
+                                assert.strictEqual(rows[0].distance, undefined);
+                                resolve();
+                            })
+                            .catch((err) =>
+                            {
+                                assert.ok(!err, err);
+                                reject(err);
+                            });
+                    })
+                    .catch((err) =>
+                    {
+                        assert.ok(!err, err);
+                        reject(err);
+                    });
+            })
+            .catch((err) =>
+            {
+                assert.ok(!err, err);
+                reject(err);
+            });
+    });
 }
 
 describe('LedgerStorage', () =>
@@ -329,21 +339,25 @@ describe('Tests that sending a pre-image', () =>
         {
             assert.ok(!err1, err1?.message);
 
-            putAllBlockData(ledger_storage, (err2: Error | null) =>
-            {
-                assert.ok(!err2, err2?.message);
-
-                ledger_storage.getEnrollments(height)
-                    .then(() =>
-                    {
-                        doneIt();
-                    })
-                    .catch((err) =>
-                    {
-                        assert.ok(!err, err);
-                        doneIt();
-                    });
-            });
+            putAllBlockData(ledger_storage)
+                .then(() =>
+                {
+                    ledger_storage.getEnrollments(height)
+                        .then(() =>
+                        {
+                            doneIt();
+                        })
+                        .catch((err) =>
+                        {
+                            assert.ok(!err, err);
+                            doneIt();
+                        });
+                })
+                .catch((err) =>
+                {
+                    assert.ok(!err, err);
+                    doneIt();
+                });
         })
     });
 
