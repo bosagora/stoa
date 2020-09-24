a69da1306888ef6164fd491f45da7be415aad2d7
Geod24 <pro.mathias.lang@gmail.com>
2020-09-24 14:27:35 +0900

    Allow to configure Agora through AGORA_ENDPOINT

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { logger } from './modules/common/Logger';
 import Stoa from './Stoa';
 
 import express from 'express';
+import { URL } from 'url';
 
 const port: number = Number(process.env.PORT) || 3836;
-const stoa: express.Application = new Stoa("database", "127.0.0.1", "2826").stoa;
+const agora_address: URL = new URL(process.env.AGORA_ENDPOINT || "http://127.0.0.1:2826");
+// TODO: Replace hardcoded "127.0.0.1" with  agora_address.hostname (#106)
+const stoa: express.Application = new Stoa("database", "127.0.0.1", agora_address.port).stoa;
 
 stoa.listen(port, () => logger.info(`Stoa API server listening at ${port}`))
 .on('error', err => logger.error(err));
