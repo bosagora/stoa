44cc3b0804ebb0ebb5647ce3f822b8c6b4fe0e19
Michael Kim <michael.kim.200518@gmail.com>
2020-10-07 10:24:38 +0900

    Implement the ability to receive options through the arguments
    Used as follow.
    npm start -- --agora=http://127.0.0.1:4826/
    Used in the docker as follow.
    docker run -d -p 3836:3836 -i -t stoa -- --agora=http://127.0.0.1:4826/
    Additional arguments.
    '-a', '--agora' - The endpoint of Agora
    '-p', '--port' - The network port of Stoa
    '-d', '--database’ - The file name of sqlite3 database

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,27 @@ import 'source-map-support/register';
 import { logger } from './modules/common/Logger';
 import Stoa from './Stoa';
 
+import { ArgumentParser } from "argparse";
 import express from 'express';
 import { URL } from 'url';
 
-const port: number = Number(process.env.PORT) || 3836;
-const agora_address: URL = new URL(process.env.AGORA_ENDPOINT || "http://127.0.0.1:2826");
-// TODO: Replace hardcoded "127.0.0.1" with  agora_address.hostname (#106)
-const stoa: express.Application = new Stoa("database", "127.0.0.1", agora_address.port).stoa;
+// Parse the arguments
+const parser = new ArgumentParser();
+parser.add_argument('-a', '--agora', { help: "The endpoint of Agora" });
+parser.add_argument('--address', { help: "The address to which we bind to Stoa" });
+parser.add_argument('-p', '--port', { help: "The port on which we bind to Stoa" });
+parser.add_argument('-d', '--database', { help: "The file name of sqlite3 database" });
+let args = parser.parse_args();
 
-stoa.listen(port, () => logger.info(`Stoa API server listening at ${port}`))
+const address: string = args.address || "0.0.0.0";
+const port: number = Number(args.port || "3836");
+const agora_address: URL = new URL(args.agora || "http://127.0.0.1:2826");
+const database_filename: string = args.database || "database";
+logger.info(`Using Agora located at: ${agora_address.hostname}: ${agora_address.port}`);
+logger.info(`The address to which we bind to Stoa: ${address}`);
+logger.info(`The port to which we bind to Stoa: ${port}`);
+logger.info(`The file name of sqlite3 database: ${database_filename}`);
+const stoa: express.Application = new Stoa(database_filename, agora_address.hostname, agora_address.port).stoa;
+
+stoa.listen(port, address, () => logger.info(`Listening to requests on: ${address}:${port}`))
 .on('error', err => logger.error(err));
