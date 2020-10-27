c641170f15b4a564f179a9a26adccec4bfbee680
Geod24 <pro.mathias.lang@gmail.com>
2020-10-27 15:29:20 +0900

    Simplify construction of `Height`
    Since the constructor accepts `bigint | string | UInt64`,
    there's no need to call either `fromString` or `fromNumber`,
    reducing the dependencies between modules.

--- tests/Utils.test.ts
+++ tests/Utils.test.ts
@@ -14,7 +14,6 @@
 import * as assert from 'assert';
 import { Utils } from '../src/modules/utils/Utils';
 import { Height } from '../src/modules/data';
-import { UInt64 } from 'spu-integer-math';
 
 describe ('Test of isInteger, isPositiveInteger, isNegativeInteger', () =>
 {
@@ -47,14 +46,14 @@ describe ('Test of toJson', () =>
 {
     it ('We can not get the desired result when UInt64 is used in `JSON.stringify`', () =>
     {
-        let height = new Height(UInt64.fromNumber(45));
+        let height = new Height(45n);
         let json = JSON.stringify(height);
         assert.strictEqual(json, '{"value":{"hi":0,"lo":45}}');
     });
 
     it ('It works well when UInt64 is used in `toJson`', () =>
     {
-        let height = new Height(UInt64.fromNumber(45));
+        let height = new Height(45n);
         let json = Utils.toJson(height);
         assert.strictEqual(json, '{"value":45}');
     });
