0dff4a5809768012cf8af901b26ab29924072be5
Geod24 <pro.mathias.lang@gmail.com>
2020-10-30 13:14:00 +0900

    Replace all usages of UInt64 with BigInt
    This allows us to write more DRY and readable code,
    while reducing dependencies.

--- src/modules/utils/Utils.ts
+++ src/modules/utils/Utils.ts
@@ -12,7 +12,6 @@
 *******************************************************************************/
 
 import * as assert from 'assert';
-import { UInt64 } from 'spu-integer-math';
 
 export class Utils
 {
@@ -44,32 +43,6 @@ export class Utils
     }
 
     /**
-     * Converts the UInt64 to the string
-     * @param value The number of UInt64
-     * @param radix An integer in the range 2 through 36 specifying
-     * the base to use for representing numeric values.
-     */
-    public static UInt64ToString(value: UInt64, radix?: number): string
-    {
-        const BIT32 = 4294967296;
-
-        let high = value.hi;
-        let low = value.lo;
-        let str = "";
-
-        radix = radix || 10;
-        while (true)
-        {
-            let mod = (high % radix) * BIT32 + low;
-            high = Math.floor(high / radix);
-            low = Math.floor(mod / radix);
-            str = (mod % radix).toString(radix) + str;
-            if (!high && !low) break;
-        }
-        return str;
-    }
-
-    /**
      * It is a function that outputs an object with BigInt as JSON.
      * @param data - Object to be converted to a JSON string
      * @returns - If the data type is UInt64 then it converts
@@ -86,11 +59,6 @@ export class Utils
         // it converts it into a string and adds '#uint64' after that.
         const json = JSON.stringify(data, (_, value) =>
         {
-            if (value instanceof UInt64)
-            {
-                uint64_to_string++;
-                return `${Utils.UInt64ToString(value)}#uint64`;
-            }
             return value;
         });
 
