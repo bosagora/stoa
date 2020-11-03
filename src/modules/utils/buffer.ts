8a6fd4089ab4f19029b0b8e906fa6393d14e5069
Geod24 <pro.mathias.lang@gmail.com>
2020-11-03 12:19:40 +0900

    Remove custom 'reverse' function
    Since we're already working with Node.js' Buffer class,
    which is itself a subclass of Javascript's UInt8Array,
    we can just use the in-place reverse that comes as
    part of the superclass prototype.

--- src/modules/utils/buffer.ts
+++ src/modules/utils/buffer.ts
@@ -76,27 +76,3 @@ export function writeToString (source: Buffer, endian?: Endian): string
     else
         return '0x' + source.toString("hex");
 }
-
-/**
- * Reverse the Buffer
- * @param source The source buffer
- * @param target The target buffer
- */
-export function reverse (source: Buffer, target?: Buffer): Buffer
-{
-    if (target == undefined)
-        target = Buffer.alloc(source.length);
-    else
-        assert.strictEqual(source.length, target.length);
-
-    let start = 0;
-    let end = source.length - 1;
-    while (start < end)
-    {
-        target[start] = source[end];
-        target[end] = source[start];
-        start++;
-        end--;
-    }
-    return target;
-}
