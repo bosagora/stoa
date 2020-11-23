3edddd75a1f700877bbc2fb1f1db94f03c71acc8
Michael Kim <michael.kim.200518@gmail.com>
2020-11-23 15:54:04 +0900

    Separate types associated with Stoa API responses from common types
    The purpose of this is one of the tasks required to package common data

--- src/Types.ts
+++ src/Types.ts
@@ -1,5 +1,58 @@
+/*******************************************************************************
+
+    Contains the type of data provided by the Stoa API
+
+    Copyright:
+        Copyright (c) 2020 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+ *******************************************************************************/
+
+import { Height } from './modules/data';
+
 /**
- * The interface of the UTXO for Stoa API response
+ * The interface of the Validator
+ */
+export interface IValidator
+{
+    address: string;
+    enrolled_at: Height;
+    stake: string;
+    preimage?: IPreimage;
+}
+
+/**
+ * The interface of the Preimage
+ */
+export interface IPreimage
+{
+    distance?: number;
+    hash?: string;
+}
+
+/**
+ * The class of the Validator data
+ */
+export class ValidatorData implements IValidator
+{
+    address: string;
+    enrolled_at: Height;
+    stake: string;
+    preimage: IPreimage;
+
+    constructor(address: string, enrolled_at: Height, stake: string, preimage: IPreimage) {
+        this.address = address;
+        this.enrolled_at = enrolled_at;
+        this.stake = stake;
+        this.preimage = preimage;
+    }
+}
+
+/**
+ * The interface of the UTXO
  */
 export interface IUnspentTxOutput
 {
