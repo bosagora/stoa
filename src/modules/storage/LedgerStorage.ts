9a43dcf2d28b10ce516fefa00751de14147e857a
Michael Kim <michael.kim.200518@gmail.com>
2021-01-20 17:53:13 +0900

    Wallet: Add the lock data in the transaction output (/utxo)

--- src/modules/storage/LedgerStorage.ts
+++ src/modules/storage/LedgerStorage.ts
@@ -1049,6 +1049,8 @@ export class LedgerStorage extends Storages
             `SELECT
                 O.utxo_key as utxo,
                 O.amount,
+                O.lock_type,
+                O.lock_bytes,
                 T.block_height,
                 B.time_stamp as block_time,
                 T.type,
