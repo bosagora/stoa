ac4fb888586ea0fdc3f7aefacdc3d027c2704f6b
Michael Kim <michael.kim.200518@gmail.com>
2021-08-09 07:56:12 +0900

    Fix a bug - Certain variables can be undefined.

--- src/modules/middleware/blacklistMiddleware.ts
+++ src/modules/middleware/blacklistMiddleware.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from 'express';
-import { logger, Logger } from '../common/Logger';
+import { Request, Response, NextFunction } from "express";
+import { logger, Logger } from "../common/Logger";
 
 /**
  * Middleware to check the blacklist IP
@@ -7,14 +7,20 @@ import { logger, Logger } from '../common/Logger';
  */
 export const isBlackList = async (req: Request, res: Response, next: NextFunction) => {
     let ipAddress = String(req.ip);
-    let db = Logger.dbInstance.connection.db;
-    if (!db)
+    if (
+        Logger.dbInstance === undefined ||
+        Logger.dbInstance.connection === undefined ||
+        Logger.dbInstance.connection.db
+    ) {
         next();
-    let isBlackListed = await db.collection('blacklists').findOne({ ipAddress });
-    if (!isBlackListed)
-        next();
-    else {
-        res.status(403).send('Your request has been rejected.')
-        logger.warn(`Error: IP:${ipAddress} is blocked.`)
+        return;
+    } else {
+        let db = Logger.dbInstance.connection.db;
+        let isBlackListed = await db.collection("blacklists").findOne({ ipAddress });
+        if (!isBlackListed) next();
+        else {
+            res.status(403).send("Your request has been rejected.");
+            logger.warn(`Error: IP:${ipAddress} is blocked.`);
+        }
     }
-}
+};
